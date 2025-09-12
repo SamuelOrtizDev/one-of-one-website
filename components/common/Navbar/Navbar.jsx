b8ad1576db4cce6fac0ea0976298c82bf53299e0
Samuel Ortiz <samuelortizwork4@gmail.com>
2025-09-12 12:22:47 -0500

    fix: hide gift a memory option

--- components/common/Navbar/Navbar.jsx
+++ components/common/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Navbar() {
                 </Link>
 
                 <nav className="hidden md:flex items-center gap-8 text-blue-100">
-                    <Link className={linkStates} href="/">Gift a Memory</Link>
+                    {/* <Link className={linkStates} href="/">Gift a Memory</Link> */}
                     <Link className={linkStates} href="/">About Us</Link>
                     <Link href="/create-your-inspiration" className="rounded-full font-bold transition-all ease-in-out duration-300 cursor-pointer hover:brightness-110 hover:saturate-200 py-2 px-6 bg-gradient-to-r from-oOrange-100 to-oOrange-200 text-white">Create Your inspiration</Link>
                 </nav>
@@ -37,7 +37,7 @@ export default function Navbar() {
                 {
                     isOpen &&
                     <div className="flex flex-col items-center gap-4 basis-full mb-8 text-blue-100">
-                        <Link className={linkStates} href="/">Gift a Memory</Link>
+                        {/* <Link className={linkStates} href="/">Gift a Memory</Link> */}
                         <Link className={linkStates} href="/">About Us</Link>
                         <Link href="/create-your-inspiration" className="rounded-full font-bold transition-all ease-in-out duration-300 cursor-pointer hover:brightness-110 hover:saturate-200 py-2 px-6 bg-gradient-to-r from-oOrange-100 to-oOrange-200 text-white">Create Your inspiration</Link>
                     </div>
