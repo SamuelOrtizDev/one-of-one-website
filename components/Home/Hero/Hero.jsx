16469572e036ba770fe5c33fa1aab5a3ab0c8988
Samuel Ortiz <samuelortizwork4@gmail.com>
2025-09-25 15:46:30 -0500

    fix: new hero copy

--- components/Home/Hero/Hero.jsx
+++ components/Home/Hero/Hero.jsx
@@ -14,7 +14,7 @@ export function Hero() {
             }}
             noPadding className='flex flex-col items-center text-center gap-6 justify-center py-32 mt-16 md:mt-20 text-white'>
             <h1 className="font-bold text-3xl md:text-6xl">Craft Your Inspiration</h1>
-            <p className="font-semibold text-xl md:text-2xl">Where Words Meet Timeless Imagery</p>
+            <p className="font-semibold text-xl md:text-2xl">one-of-a-kind custom posters</p>
             <Link href="/create-your-inspiration" className="rounded-full font-bold transition-all ease-in-out duration-300 cursor-pointer hover:brightness-110 hover:saturate-200 w-full max-w-[350px] py-2 bg-gradient-to-r from-oOrange-100 to-oOrange-200 group flex items-center justify-center gap-4">
                 Begin Your Journey
                 <span className="group-hover:translate-x-1 transition-transform">
