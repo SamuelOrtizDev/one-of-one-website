bfeaff7687b6a62d959b3d774793911a639864af
Samuel Ortiz <samuelortizwork4@gmail.com>
2025-10-29 18:04:25 -0500

    feat: add section on about us page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,13 @@
+import dynamic from "next/dynamic";
+
+const GetStarted = dynamic(() =>
+    import("@/components/Home/GetStarted/Main").then(mod => ({ default: mod.GetStarted }))
+);
+
 export default function About() {
     return (
-        <p>aqui va</p>
+        <>
+            <GetStarted/>
+        </>
     )
 }
