edc5517b327a9214d4585cae5b804ed1fef88616
Samuel Ortiz <samuelortizwork4@gmail.com>
2025-10-30 23:16:02 -0500

    fix: more padding on contact page for mobile

--- components/Home/StillHaveQuestions/Main.jsx
+++ components/Home/StillHaveQuestions/Main.jsx
@@ -9,7 +9,7 @@ import formBg from "./assets/formBg.webp"
 export function StillHaveQuestions({isContactPage = false}) {
 
     return (
-        <SectionLayout noPadding containerClasses={"bg-gradient-to-b from-obsidian-100 to-obsidian-200"} className={`text-white flex flex-col gap-8 relative overflow-hidden ${isContactPage ? "pb-12 md:pb-20 pt-20 md:pt-56" : "py-12 md:py-20"}`}>
+        <SectionLayout noPadding containerClasses={"bg-gradient-to-b from-obsidian-100 to-obsidian-200"} className={`text-white flex flex-col gap-8 relative overflow-hidden ${isContactPage ? "pb-12 md:pb-20 pt-28 md:pt-56" : "py-12 md:py-20"}`}>
             <span className="flex flex-col gap-4 md:gap-6">
                 <motion.h2 {...fadeInUp} className="text-3xl md:text-5xl">{!isContactPage && "Still "}Have <span className="px-2 py-2 text-white font-bold bg-oBlue-100">Questions?</span></motion.h2>
                 <motion.p {...fadeInUp}>We're here to help you create something meaningful.</motion.p>
